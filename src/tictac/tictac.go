3158529356cc24b9308aae8183d4b637d81d763e
thenerdjake <thenerdjake@users.noreply.github.com>
2019-05-25 13:24:24 -0500

    initialize tictac.go and built board and helper functions

---
+++ src/tictac/tictac.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var a, b, c, d, e, f, g, h, i = " ", " ", " ", " ", " ", " ", " ", " ", " "
+	var s = " | "
+	var line = "-----------"
+	var line1 = a + s + b + s + c
+	var line2 = d + s + e + s + f
+	var line3 = g + s + h + s + i
+
+	fmt.Println(line1)
+	fmt.Println(line)
+	fmt.Println(line2)
+	fmt.Println(line)
+	fmt.Println(line3)
+
+	//Choose wether to be X's or O's
+	var team string
+	for {
+		team = chooseTeam()
+		if team == "x" || team == "X" || team == "O" || team == "o" {
+			break
+		}
+		fmt.Println("Please choose either X's or O's")
+	}
+}
+
+//function to ask user to choose a team
+//return string team
+func chooseTeam() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Do you wanna be X's or O's?")
+	var team string
+	team, _ = reader.ReadString('\n')
+	team = strings.TrimRight(team, "\r\n")
+	return team
+}
+
+//ask the player how many people are playing
+//ToDo: give option of 0,1,2
+//return string playnumber
+func choosePlayers() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("How many Players?")
+	var playerNumber string
+	playerNumber, _ = reader.ReadString('\n')
+	playerNumber = strings.TrimRight(playerNumber, "\r\n")
+	return playerNumber
+}
+
+//ask the player to answer yes or no if they want to go first
+//return string playerFirst
+func goFirst() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Do you wanna go first? yes or no?")
+	var playerFirst string
+	playerFirst, _ = reader.ReadString('\n')
+	playerFirst = strings.TrimRight(playerFirst, "\r\n")
+	return playerFirst
+}
