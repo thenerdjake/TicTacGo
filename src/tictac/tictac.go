ad4e16118d3fe47d4a9e6b1a3c79b837f18fc121
thenerdjake <thenerdjake@users.noreply.github.com>
2019-11-17 21:58:58 -0600

    it now plays but never checks if you win or tie

--- src/tictac/tictac.go
+++ src/tictac/tictac.go
@@ -12,6 +12,8 @@ var board = [3][3]string{
 	{" ", " ", " "},
 	{" ", " ", " "}}
 
+	var currentPlayer = " "
+
 func main() {
 
 	printboard()
@@ -47,8 +49,20 @@ func main() {
 		}
 	}
 
-	changePlayer(team)
-	//
+	if first == true{
+		setPlayer(team)
+	}else {
+		setPlayer(opposite(team))
+	}
+
+	for {
+		playGame()
+		if (playAgain() == false){
+			break
+		}
+	}
+
+
 
 }
 
@@ -61,6 +75,22 @@ func chooseTeam() string {
 	team, _ = reader.ReadString('\n')
 	team = strings.TrimRight(team, "\r\n")
 	return team
+}
+
+func capitalize(letter string) string{
+	if letter == "x" || letter == "X"{
+		return "X"
+	}else {
+		return "O"
+	}
+}
+
+func opposite(letter string) string{
+		if letter == "X"{
+			return "O"
+		}else {
+			return "X"
+		}
 }
 
 //ask the player how many people are playing
@@ -95,6 +125,18 @@ func clearBoard() {
 
 }
 
+func playGame() {
+	for {
+		if checkWin() == true{
+			break
+		}
+		askInput()
+		printboard()
+		changePlayer()
+	}
+	displayWinner()
+}
+
 func updateBoard(xlocation int, ylocation int, tile string) {
 	board[xlocation][ylocation] = tile
 }
@@ -108,23 +150,57 @@ func printboard() {
 }
 
 func placeTile(location int, tile string) {
-	var xlocation = (location / 3) - 1
-	var ylocation = (location % 3) - 1
+	xlocation := (location - 1) / 3
+	ylocation := (location -1) % 3
 	updateBoard(xlocation, ylocation, tile)
 }
 
-func changePlayer(team string){
-	if team == "X" {
-		team = "O"
-	} else team == "O" {
-		team = "X"
+func setPlayer(team string){
+	currentPlayer = capitalize(team)
+}
+
+func changePlayer(){
+	if currentPlayer == "X" {
+		currentPlayer = "O"
+	} else if currentPlayer== "O" {
+		currentPlayer = "X"
 	}
 }
 
-func checkWin() {}
+func checkWin() bool {
+	return false
+}
+
+func playAgain() bool {
+	return false
+}
 
 func askInput() {
+	for{
+		fmt.Println("Current Player: " + currentPlayer)
+		fmt.Println("Where do you want to go? 1-9")
+		var choice int
+		fmt.Scan(&choice)
+		if choice < 0 && choice > 10 {
+				fmt.Println("I said 1-9, try again")
+		}else if checkTile(choice) != " "{
+			fmt.Println("That spot is taken try again")
+		}else {
+			placeTile(choice, currentPlayer)
+			break
+		}
+	}
 
 }
 
+func checkTile(location int) string{
+	xlocation := (location - 1) / 3
+	ylocation := (location -1) % 3
+	return board[xlocation][ylocation]
+}
+
 func error() {}
+
+func checkPlayAgain() {}
+
+func displayWinner() {}
